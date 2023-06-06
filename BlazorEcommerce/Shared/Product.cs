e849a3a8afb0e5e83776e3412093eb18e46286e1
LeeTrent <lee.trent.1@gmail.com>
2023-06-06 08:06:27 -0500

    Got product list working

---
+++ BlazorEcommerce/Shared/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorEcommerce.Shared
+{
+    public class Product
+    {
+
+#pragma warning disable CS8618
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string ImageUrl { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+#pragma warning restore CS8618 
+    }
+}
