b975a2e10c366ccdcd6465cd5abdbbba1bcda664
LeeTrent <lee.trent.1@gmail.com>
2023-06-15 20:28:38 -0500

    Now using code behind classes for ProductList and ProductDetails

---
+++ BlazorEcommerce/Client/Pages/ProductDetails.razor.cs
@@ -0,0 +1,36 @@
+﻿using BlazorEcommerce.Shared;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace BlazorEcommerce.Client.Pages
+{
+    public partial class ProductDetails
+    {
+        private Product? product = null;
+        private string message = String.Empty;
+
+        [Parameter]
+        public int Id { get; set; }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            this.message = "Loading product...";
+            ServiceResponse<Product>? result = await Http.GetFromJsonAsync<ServiceResponse<Product>>($"api/product/{Id}");
+            if (result == null)
+            {
+                this.message = "Something went wrong.";
+            }
+            else
+            {
+                if (result.Success)
+                {
+                    this.product = result.Data;
+                }
+                else
+                {
+                    this.message = result.Message;
+                }
+            }
+        }
+    }
+}
---
+++ BlazorEcommerce/Client/Shared/ProductList.razor.cs
@@ -0,0 +1,19 @@
+﻿using BlazorEcommerce.Shared;
+using System.Net.Http.Json;
+
+namespace BlazorEcommerce.Client.Shared
+{
+    public partial class ProductList
+    {
+        private List<Product> Products = new();
+
+        protected override async Task OnInitializedAsync()
+        {
+            var result = await Http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product");
+            if (result != null && result.Data != null)
+            {
+                this.Products = result.Data;
+            }
+        }
+    }
+}
