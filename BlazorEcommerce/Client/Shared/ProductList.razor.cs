491e96b706d631eaa771f299d725fd3b1139dbe5
LeeTrent <lee.trent.1@gmail.com>
2023-06-17 11:07:04 -0500

    Added 'Loading products...' span message when loading products

--- BlazorEcommerce/Client/Shared/ProductList.razor.cs
+++ BlazorEcommerce/Client/Shared/ProductList.razor.cs
@@ -8,13 +8,16 @@ namespace BlazorEcommerce.Client.Shared
     {
         [Inject] private HttpClient? WebClient { get; set; }
         private List<Product> Products = new();
+        private string Message { get; set; } = String.Empty;
 
         protected override async Task OnInitializedAsync()
         {
+            this.Message = "Loading products...";
             var result = await WebClient!.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product");
             if (result != null && result.Data != null)
             {
                 this.Products = result.Data;
+                this.Message = String.Empty;
             }
         }
     }
