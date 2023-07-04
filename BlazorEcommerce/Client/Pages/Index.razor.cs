d32253a3eb430b09083820c9180298efcd0e79c7
LeeTrent <lee.trent.1@gmail.com>
2023-07-04 10:09:56 -0500

    now filtering on product category in the child component ProductList.razor

--- BlazorEcommerce/Client/Pages/Index.razor.cs
+++ BlazorEcommerce/Client/Pages/Index.razor.cs
@@ -1,40 +1,10 @@
-﻿using BlazorEcommerce.Client.Services.ProductService;
-using BlazorEcommerce.Shared;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
 
 namespace BlazorEcommerce.Client.Pages
 {
     public partial class Index
     {
-        [Inject]
-        private IProductServiceClient? ProductServiceClient { get; set; }
-
         [Parameter]
-        public string? ProductCategory { get; set; }
-
-        private List<Product> Products = new();
-        private string Message = String.Empty;
-
-        protected override async Task OnInitializedAsync()
-        {
-            await LoadProducts();
-        }
-
-        protected override async Task OnParametersSetAsync()
-        {
-            await base.OnParametersSetAsync();
-            await LoadProducts();
-        }
-
-        private async Task LoadProducts()
-        {
-            this.Message = "Loading products...";
-            var result = await ProductServiceClient!.GetProducts(this.ProductCategory);
-            if (result != null && result.Data != null)
-            {
-                this.Products = result.Data;
-                this.Message = String.Empty;
-            }
-        }
+        public string? productCategory { get; set; }
     }
 }
--- BlazorEcommerce/Client/Shared/ProductList.razor.cs
+++ BlazorEcommerce/Client/Shared/ProductList.razor.cs
@@ -9,13 +9,28 @@ namespace BlazorEcommerce.Client.Shared
         [Inject]
         private IProductServiceClient? ProductServiceClient { get; set; }
 
+        [Parameter]
+        public string? Category { get; set; }
+
         private List<Product> Products = new();
         private string Message = String.Empty;
 
         protected override async Task OnInitializedAsync()
         {
+            await LoadProducts();
+        }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            await base.OnParametersSetAsync();
+            await LoadProducts();
+        }
+
+
+        private async Task LoadProducts()
+        {
             this.Message = "Loading products...";
-            var result = await ProductServiceClient!.GetProducts();
+            var result = await ProductServiceClient!.GetProducts(this.Category);
             if (result != null && result.Data != null)
             {
                 this.Products = result.Data;
