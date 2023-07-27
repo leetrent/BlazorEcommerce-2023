f7749cbe787ec568da0a8209d324afbf5dae816d
LeeTrent <lee.trent.1@gmail.com>
2023-07-27 17:17:31 -0500

    Added product type and product variants tables

--- BlazorEcommerce/Shared/Product.cs
+++ BlazorEcommerce/Shared/Product.cs
@@ -8,23 +8,23 @@ namespace BlazorEcommerce.Shared
 
 #pragma warning disable CS8618
         public int Id { get; set; }
-        [Required]
-        public string Title { get; set; }
-        [Required]
-        public string Description { get; set; }
-        [Required]
-        public string ImageUrl { get; set; }
 
         [Required]
-        [Column(TypeName = "decimal(18,2)")]
-        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than $0.00.")]
-        public decimal Price { get; set; }
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
 
         [Required]
         [Range(1, int.MaxValue, ErrorMessage = "CategoryId must be greater than zero.")]
         public int CategoryId { get; set; }
 
         public Category Category { get; set; }
+
+        public List<ProductVariant> Variants { get; set; } = new();
 
 #pragma warning restore CS8618 
     }
